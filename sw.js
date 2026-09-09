51fac9a18bda0b23278f5011a7676dfeb907da45
anqiuzz1 <15327105717@189.cn>
2026-09-09 20:06:13 +0800

    fix: 更新sw缓存版本为jintian-v20260909-3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 今天 App Service Worker - 离线缓存 + Web Push */
-const CACHE_NAME='jintian-v20260909-2';
+const CACHE_NAME='jintian-v20260909-3';
 const ASSETS=['./','./index.html'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE_NAME).then(function(c){return c.addAll(ASSETS)})),self.skipWaiting()});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE_NAME}).map(function(k){return caches.delete(k)}))})),self.clients.claim()});
